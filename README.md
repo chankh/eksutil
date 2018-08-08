dc0fa24d0c4d52ef5e17827b3763f39a57c143ef
KH Chan <kwunhok@gmail.com>
2018-08-08 21:03:59 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Lambda Lambda Sample
+=======
+
+This code repository aims to be a reference sample for implementing AWS Lambda function to interact
+with Amazon Elastic Container Services for Kubernetes (EKS) using Kubernetes API. Amazon EKS runs
+the Kubernetes management infrastructure for you across multiple AWS availability zones to eliminate
+a single point of failure. Amazon EKS is certified Kubernetes conformant so you can use existing
+tooling and plugins from partners and the Kubernetes community. Applications running on any standard
+Kubernetes environment are fully compatible and can be easily migrated to Amazon EKS.
+
+Having a AWS Lambda function that integrates with Amazon EKS would allow event-driven triggers to
+manage and modify deployments and other controls within the Amazon EKS cluster.
+
+While this is just a sample that list the pods running within the Amazon EKS cluster, you can
+modify the code to meet yourneeds.
+
+Setting this up is rather easy. Build the function using the `make` command.
+
+```
+$ make
+```
+
+This creates a zip package of the function which can be deployed to AWS Lambda. There are few
+environment variables that will configure how this function works.
+
+Variable Name | Description
+--------------|------------
+CLUSTER_NAME | Name of the Amazon EKS cluster.
+ENV | (Optional) Setting this variable to `PRODUCTION` controls the level of log output.
+
+You would also need to give the Lambda execution role permissions in Amazon EKS cluster. Refer to
+this [User Guide](https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html) for detailed
+instructions.
+
+1. Edit the `aws-auth` ConfigMap of your cluster.
+```
+$ kubectl -n kube-system edit configmap/aws-auth
+```
+2. Add your Lambda execution role to the config
+```
+# Please edit the object below. Lines beginning with a '#' will be ignored,
+# and an empty file will abort the edit. If an error occurs while saving this file will be
+# reopened with the relevant failures.
+#
+apiVersion: v1
+data:
+  mapRoles: |
+    - rolearn: arn:aws:iam::555555555555:role/devel-worker-nodes-NodeInstanceRole-74RF4UBDUKL6
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    - rolearn: arn:aws:iam::<AWS Account ID>:role/<your lambda execution role>
+      username: admin
+      groups:
+        - system:masters
+```
+
+Once these are configured, you can test your function. Good luck!
+
+
+
---
+++ main.go
@@ -0,0 +1,252 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/kubernetes-sigs/aws-iam-authenticator/pkg/token"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go/service/sts/stsiface"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+func main() {
+	if os.Getenv("ENV") != "PRODUCTION" {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	lambda.Start(handler)
+}
+
+func handler(context context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Start a new AWS session
+	s := newSession()
+
+	// Setup the basic EKS cluster info
+	cfg := &ClusterConfig{
+		ClusterName: os.Getenv("CLUSTER_NAME"),
+		Session:     s,
+	}
+
+	// Load the rest from AWS using SDK
+	cfg.loadConfig()
+
+	// Create the Kubernetes client
+	client, err := cfg.NewClientConfig()
+	if err != nil {
+		log.WithError(err).Fatal(err.Error())
+	}
+
+	clientset, err := client.NewClientSetWithEmbeddedToken()
+	if err != nil {
+		log.WithError(err).Fatal(err.Error())
+	}
+
+	// Call Kubernetes API here
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		log.WithError(err).Fatal("Error listing pods")
+	}
+
+	var results []string
+
+	for i, pod := range pods.Items {
+		log.Infof("[%d] %s", i, pod.Name)
+		results = append(results, pod.Name)
+	}
+
+	json, err := json.Marshal(results)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to marshal results to json")
+
+	}
+
+	return events.APIGatewayProxyResponse{Body: string(json), StatusCode: 200}, nil
+}
+
+// Retrieve EKS cluster endpoint and CA from AWS
+func (c *ClusterConfig) loadConfig() {
+	svc := eks.New(c.Session)
+	input := &eks.DescribeClusterInput{
+		Name: aws.String(c.ClusterName),
+	}
+
+	log.WithField("cluster", c.ClusterName).Info("Looking up EKS cluster")
+
+	result, err := svc.DescribeCluster(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			errors.Wrap(err, aerr.Error())
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			errors.Wrap(err, err.Error())
+		}
+	}
+
+	log.WithField("cluster", c.ClusterName).Info("Found cluster")
+	log.WithField("cluster", result.Cluster).Debug("Cluster details")
+
+	c.MasterEndpoint = *result.Cluster.Endpoint
+	c.CertificateAuthorityData = *result.Cluster.CertificateAuthority.Data
+}
+
+func (c *ClusterConfig) NewClientConfig() (*ClientConfig, error) {
+
+	stsAPI := sts.New(c.Session)
+
+	iamRoleARN, err := checkAuth(stsAPI)
+	if err != nil {
+		return nil, err
+	}
+	contextName := fmt.Sprintf("%s@%s", getUsername(iamRoleARN), c.ClusterName)
+
+	data, err := base64.StdEncoding.DecodeString(c.CertificateAuthorityData)
+	if err != nil {
+		return nil, errors.Wrap(err, "decoding certificate authority data")
+	}
+
+	log.Info("Creating Kubernetes client config")
+	clientConfig := &ClientConfig{
+		Client: &clientcmdapi.Config{
+			Clusters: map[string]*clientcmdapi.Cluster{
+				c.ClusterName: {
+					Server: c.MasterEndpoint,
+					CertificateAuthorityData: data,
+				},
+			},
+			Contexts: map[string]*clientcmdapi.Context{
+				contextName: {
+					Cluster:  c.ClusterName,
+					AuthInfo: contextName,
+				},
+			},
+			AuthInfos: map[string]*clientcmdapi.AuthInfo{
+				contextName: &clientcmdapi.AuthInfo{},
+			},
+			CurrentContext: contextName,
+		},
+		ClusterName: c.ClusterName,
+		ContextName: contextName,
+		roleARN:     iamRoleARN,
+		sts:         stsAPI,
+	}
+
+	return clientConfig, nil
+
+}
+
+func newSession() *session.Session {
+	config := aws.NewConfig()
+	config = config.WithCredentialsChainVerboseErrors(true)
+
+	opts := session.Options{
+		Config:                  *config,
+		SharedConfigState:       session.SharedConfigEnable,
+		AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
+	}
+
+	stscreds.DefaultDuration = 30 * time.Minute
+
+	return session.Must(session.NewSessionWithOptions(opts))
+}
+
+func checkAuth(stsAPI stsiface.STSAPI) (string, error) {
+	input := &sts.GetCallerIdentityInput{}
+	output, err := stsAPI.GetCallerIdentity(input)
+	if err != nil {
+		return "", errors.Wrap(err, "checking AWS STS access – cannot get role ARN for current session")
+	}
+	iamRoleARN := *output.Arn
+	log.Debugf("role ARN for the current session is %s", iamRoleARN)
+	return iamRoleARN, nil
+}
+
+type ClusterConfig struct {
+	ClusterName              string
+	MasterEndpoint           string
+	CertificateAuthorityData string
+	Session                  *session.Session
+}
+
+type ClientConfig struct {
+	Client      *clientcmdapi.Config
+	ClusterName string
+	ContextName string
+	roleARN     string
+	sts         stsiface.STSAPI
+}
+
+func getUsername(iamRoleARN string) string {
+	usernameParts := strings.Split(iamRoleARN, "/")
+	if len(usernameParts) > 1 {
+		return usernameParts[len(usernameParts)-1]
+	}
+	return "iam-root-account"
+}
+
+func (c *ClientConfig) WithEmbeddedToken() (*ClientConfig, error) {
+	clientConfigCopy := *c
+
+	log.Info("Generating token")
+
+	gen, err := token.NewGenerator()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get token generator")
+	}
+
+	tok, err := gen.GetWithSTS(c.ClusterName, c.sts.(*sts.STS))
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get token")
+	}
+
+	x := c.Client.AuthInfos[c.ContextName]
+	x.Token = tok
+
+	log.WithField("token", tok).Debug("Successfully generated token")
+	return &clientConfigCopy, nil
+}
+
+func (c *ClientConfig) NewClientSetWithEmbeddedToken() (*clientset.Clientset, error) {
+	clientConfig, err := c.WithEmbeddedToken()
+	if err != nil {
+		return nil, errors.Wrap(err, "creating Kubernetes client config with embedded token")
+	}
+	clientSet, err := clientConfig.NewClientSet()
+	if err != nil {
+		return nil, errors.Wrap(err, "creating Kubernetes client")
+	}
+	return clientSet, nil
+}
+
+func (c *ClientConfig) NewClientSet() (*clientset.Clientset, error) {
+	clientConfig, err := clientcmd.NewDefaultClientConfig(*c.Client, &clientcmd.ConfigOverrides{}).ClientConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create API client configuration from client config")
+	}
+
+	client, err := clientset.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create API client")
+	}
+	return client, nil
+}
